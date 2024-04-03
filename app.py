aeeb1a3e046fe8e50cf954e061c00c6f999dd127
zih-an <annyang.p@gmail.com>
2024-04-03 17:50:56 +0100

    added recorders

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!\n"
+def home():
+    return render_template("index.html", ind=1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
